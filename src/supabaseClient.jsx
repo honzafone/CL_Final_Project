494df918fcca4afb45555dd87ea6fecb39ed7469
Jan Decker <Jan.Decker23@gmail.com>
2024-10-29 21:17:49 +0100

    version_supabase_cchanges

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -1,10 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-
-
-const supabaseUrl = 'https://lvoizjagakphmepozvpu.supabase.co'; // Vaše URL z Supabase
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2b2l6amFnYWtwaG1lcG96dnB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwNjEzMTYsImV4cCI6MjA0NTYzNzMxNn0.i5eTx8cpLz5dUVAjZSPvtNGNp4BCTSj8sHJJYFIgfYY'; // Váš Anon Public Key z Supabase
-console.log(supabaseUrl, supabaseKey);
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// Create a single supabase client for interacting with your database
+export const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_API_KEY);
